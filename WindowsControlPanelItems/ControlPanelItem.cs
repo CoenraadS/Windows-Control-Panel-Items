5897e2cc2cbe980d940f2d89be0c03ecea63578b
CoenraadS <coenraad@y7mail.com>
2014-07-16 12:12:48 +0200

    Returns a list of Panel objects

---
+++ WindowsControlPanelItems/ControlPanelItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WindowsControlPanelItems
+{
+    public class ControlPanelItem
+    {
+        public string localizedString { get; private set; }
+        public string infoTip { get; private set; }
+        public string applicationName { get; private set; }
+        public ProcessStartInfo executablePath { get; private set; }
+
+        public ControlPanelItem(string newLocalizedString, string newInfoTip, string newApplicationName, ProcessStartInfo newExecutablePath)
+        {
+            localizedString = newLocalizedString;
+            infoTip = newInfoTip;
+            applicationName = newApplicationName;
+            executablePath = newExecutablePath;
+        }
+    }
+}
